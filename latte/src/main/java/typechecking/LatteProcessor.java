c1fa0925e72bc9342cd99336123e9d280044f87b
Daniel <101189638+cheestree@users.noreply.github.com>
2026-05-15 15:21:47 +0100

    Removed ghost generation from extraction
    Ghost generation should be in the context addition phase instead of the extractor.

--- latte/src/main/java/typechecking/LatteProcessor.java
+++ latte/src/main/java/typechecking/LatteProcessor.java
@@ -6,7 +6,6 @@ import java.util.List;
 import context.ClassLevelMaps;
 import context.PermissionEnvironment;
 import context.SymbolicEnvironment;
-import rj_language.visitors.FieldGhostsGeneration;
 import spoon.processing.AbstractProcessor;
 import spoon.reflect.declaration.CtPackage;
 import spoon.reflect.factory.Factory;
@@ -28,7 +27,6 @@ public class LatteProcessor extends AbstractProcessor<CtPackage> {
         
         if (!visitedPackages.contains(pkg)) {
             visitedPackages.add(pkg);
-            pkg.accept(new FieldGhostsGeneration(mtc));
             pkg.accept(new LatteClassFirstPass( se, pe, mtc));
             pkg.accept(new LatteTypeChecker( se, pe, mtc));
             pkg.accept(new RefinementFirstPass( se, pe, mtc));
