3fd2c2d56ada82fda26577982654c0d73a2ad863
Daniel Carvalho <101189638+cheestree@users.noreply.github.com>
2026-05-15 15:57:20 +0100

    Added refinement tests (#5)
    Added tests to be used to verify refinement behavior.

---
+++ latte/src/test/examples/refinements/PipedWriterCorrect.java
@@ -0,0 +1,73 @@
+import specification.Borrowed;
+
+public class PipedWriterCorrect {
+    public static class PipedWriter {
+        // @Ghost 
+        boolean isConnected;
+        // @StateRefinement(to = "this.isConnected == false")
+        public PipedWriter() {
+            
+        }
+
+        /*
+        @StateRefinement(
+            from = "this.isConnected == false && reader.isConnected == false",
+            to = "this.isConnected == true && reader.isConnected == true"
+        )
+        */
+        void connect(@Borrowed PipedReader reader) {
+
+        }
+
+        /*
+        @StateRefinement(
+            from = "this.isConnected == false && reader.isConnected == false", 
+            to = "this.isConnected == true && reader.isConnected == true"
+        )
+        */
+        void write(String s) {
+
+        }
+    }
+
+
+    public static class PipedReader {
+        // @Ghost
+        boolean isConnected;
+        
+        // @StateRefinement(to = "this.isConnected == false")
+        public PipedReader() {
+
+        }
+
+        /*
+        @StateRefinement(
+            from = "this.isConnected == false && writer.isConnected == false",
+            to = "this.isConnected == true && writer.isConnected == true"
+        )
+        */
+        void connect(@Borrowed PipedWriter writer) {
+
+        }
+
+        /*
+        @StateRefinement(
+            from = "this.isConnected == true", 
+            to = "this.isConnected == true"
+        )
+        */
+        void read() {
+
+        }
+    }
+
+
+    public static void main(String[] args) {
+        PipedWriter src = new PipedWriter();
+        PipedReader snk = new PipedReader();
+
+        src.connect(snk); // connects src and snk
+        src.write("Hello, World!"); // writes to the pipe
+        snk.read(); // reads from the pipe
+    }
+}
